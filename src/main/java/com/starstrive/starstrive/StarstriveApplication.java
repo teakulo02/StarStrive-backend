69909bf2b3e2eb4dd96b9f511601aa01db1e333e
BigDaddyMM <105431068+BigDaddyMM@users.noreply.github.com>
2023-05-22 16:18:15 +0200

    start

---
+++ src/main/java/com/starstrive/starstrive/StarstriveApplication.java
@@ -0,0 +1,13 @@
+package com.starstrive.starstrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarstriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarstriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starstrive/starstrive/StarstriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starstrive.starstrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarstriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
